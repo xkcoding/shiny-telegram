66c65939eed89bd4213e8033241aa7a65ef9662f
Yangkai.Shen <237497819@qq.com>
2018-08-22 10:30:47 +0800

    :white_check_mark: Adding Selenium and PhantomJs tests.

--- back/src/test/java/com/xkcoding/shiny/spider/PhantomJsTest.java
+++ back/src/test/java/com/xkcoding/shiny/spider/PhantomJsTest.java
@@ -1,11 +1,21 @@
 package com.xkcoding.shiny.spider;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONUtil;
 import com.xkcoding.shiny.ShinyApplicationTests;
+import org.assertj.core.util.Sets;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import us.codecraft.xsoup.Xsoup;
+
+import java.util.Set;
 
 /**
  * <p>
@@ -38,17 +48,62 @@ public class PhantomJsTest extends ShinyApplicationTests {
 		//js支持
 		dcaps.setJavascriptEnabled(true);
 		//驱动支持
-		dcaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "D:\\chromedriver\\phantomjs-2.1.1-windows\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
+		dcaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/Users/yangkai.shen/Desktop/phantomjs-2.1.1-macosx/bin/phantomjs");
 
 		PhantomJSDriver driver = new PhantomJSDriver(dcaps);
 		return driver;
 	}
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
 		WebDriver driver = getPhantomJSDriver();
-		driver.get("http://www.baidu.com");
-		System.out.println(driver.getCurrentUrl());
+		// 已访问的 TAB 集合，用户存放已经访问过的 TAB
+		Set<String> windowSet = Sets.newHashSet();
+
+		// 让浏览器访问 xclient.info
+		driver.get("http://xclient.info/s/things.html?t=" + RandomUtil.simpleUUID());
+		// 软件首页 TAB
+		String oldWindow = driver.getWindowHandle();
+		windowSet.addAll(driver.getWindowHandles());
+		// 用下面代码也可以实现
+		//driver.navigate().to("http://www.baidu.com");
+		// 获取 网页的 title
+		System.out.println(" Page title is: " + driver.getTitle());
+		Document document = Jsoup.parse(driver.getPageSource());
+
+		String summary = Xsoup.compile("//*[@id=\"main\"]/div/article/div[3]/p/text()").evaluate(document).get();
+		System.err.println("【summary】: " + summary);
+
+		WebElement element = driver.findElement(By.xpath("//*[@id=\"versions\"]/table/tbody/tr[1]/td[5]/a[2]"));
+		element.click();
+		Thread.sleep(1000);
+
+		// 浏览器所有 TAB
+		Set<String> windowHandles = driver.getWindowHandles();
+		System.err.println(JSONUtil.toJsonStr(windowHandles));
+
+		// 切换 TAB，只切换到未访问过的 TAB
+		for (String windowHandle : windowHandles) {
+			if (!windowSet.contains(windowHandle)) {
+				// 切换 TAB
+				driver.switchTo().window(windowHandle);
+				// 已访问的 TAB 里添加一条记录
+				windowSet.add(windowHandle);
+				break;
+			}
+		}
+		System.err.println(driver.getCurrentUrl());
+		Document bdDocument = Jsoup.parse(driver.getPageSource());
+		String bdLink = Xsoup.compile("//*[@id=\"body\"]/div[1]/div[2]/a/@data-link").evaluate(bdDocument).get();
+		String bdKey = Xsoup.compile("//*[@id=\"body\"]/div[1]/div[2]/a/@data-clipboard-text").evaluate(bdDocument).get();
+		System.err.println("【百度】" + bdLink + "【提取码】" + bdKey);
+
+		// 回到原来的 TAB 页
+		driver.switchTo().window(oldWindow);
+		System.err.println(driver.getCurrentUrl());
+
+		Set<String> windows = driver.getWindowHandles();
+		System.err.println(JSONUtil.toJsonStr(windows));
 		driver.quit();
 	}
 }
