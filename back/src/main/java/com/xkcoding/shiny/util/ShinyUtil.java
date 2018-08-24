e9694ef245b2b67187a35b5cd2dbd2960cf3e4e2
Yangkai.Shen <237497819@qq.com>
2018-08-25 02:56:13 +0800

    :sparkles: 新增系统判断工具方法

--- back/src/main/java/com/xkcoding/shiny/util/ShinyUtil.java
+++ back/src/main/java/com/xkcoding/shiny/util/ShinyUtil.java
@@ -2,6 +2,7 @@ package com.xkcoding.shiny.util;
 
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.system.SystemUtil;
 import com.xkcoding.shiny.common.ShinyConst;
 import com.xkcoding.shiny.model.query.base.PageCondition;
 
@@ -39,5 +40,32 @@ public class ShinyUtil {
 			condition.setPageSize(ShinyConst.DEFAULT_PAGE_SIZE);
 		}
 		return condition;
+	}
+
+	/**
+	 * 是否是 Mac 系统
+	 *
+	 * @return <code>true</code> 是 <code>false</code> 否
+	 */
+	private static Boolean isMac() {
+		return SystemUtil.getOsInfo().isMac() || SystemUtil.getOsInfo().isMacOsX();
+	}
+
+	/**
+	 * 是否是 Linux 系统
+	 *
+	 * @return <code>true</code> 是 <code>false</code> 否
+	 */
+	private static Boolean isLinux() {
+		return SystemUtil.getOsInfo().isLinux();
+	}
+
+	/**
+	 * 是否是 Windows 系统
+	 *
+	 * @return <code>true</code> 是 <code>false</code> 否
+	 */
+	private static Boolean isWindows() {
+		return SystemUtil.getOsInfo().isWindows();
 	}
 }
