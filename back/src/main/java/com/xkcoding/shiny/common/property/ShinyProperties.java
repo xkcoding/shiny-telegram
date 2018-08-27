0ebff051636787a04d1ce7a0057e84a4eef355cb
Yangkai.Shen <237497819@qq.com>
2018-08-27 11:15:54 +0800

    :art: 抽取配置类

---
+++ back/src/main/java/com/xkcoding/shiny/common/property/ShinyProperties.java
@@ -0,0 +1,52 @@
+package com.xkcoding.shiny.common.property;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * <p>
+ * 自定义配置
+ * </p>
+ *
+ * @package: com.xkcoding.shiny.common.property
+ * @description： 自定义配置
+ * @author: yangkai.shen
+ * @date: Created in 2018/8/27 上午9:57
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@ConfigurationProperties(prefix = "shiny")
+@Data
+public class ShinyProperties {
+	/**
+	 * 软件名称
+	 */
+	private String name;
+
+	/**
+	 * 软件版本
+	 */
+	private String version;
+
+	/**
+	 * 版权年份
+	 */
+	private String copyrightYear;
+
+	/**
+	 * 作者
+	 */
+	private String developer;
+
+	/**
+	 * driver 路径
+	 */
+	private String driverPath;
+
+	/**
+	 * 并发软件采集的数量
+	 */
+	private Integer spiderNum;
+
+}
