6193809fa75152d29c4de4bad7835f2127e636dc
Yangkai.Shen <237497819@qq.com>
2018-08-28 20:36:37 +0800

    :sparkles: 异步任务线程池配置类

---
+++ back/src/main/java/com/xkcoding/shiny/config/AsyncConfig.java
@@ -0,0 +1,38 @@
+package com.xkcoding.shiny.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * <p>
+ * 异步任务配置
+ * </p>
+ *
+ * @package: com.xkcoding.shiny.config
+ * @description： 异步任务配置
+ * @author: yangkai.shen
+ * @date: Created in 2018/8/27 下午6:41
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(20);
+		executor.setMaxPoolSize(20);
+		executor.setQueueCapacity(25);
+		executor.setThreadNamePrefix("Async-Thread-");
+		executor.initialize();
+		return executor;
+	}
+
+}
