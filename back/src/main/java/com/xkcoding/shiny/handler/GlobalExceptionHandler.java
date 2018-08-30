6a37b9b95a9a9a35b60a450c3020d86972221a9c
Yangkai.Shen <237497819@qq.com>
2018-08-30 22:55:37 +0800

    :sparkles: 新增 @PathVariable 参数异常处理

--- back/src/main/java/com/xkcoding/shiny/handler/GlobalExceptionHandler.java
+++ back/src/main/java/com/xkcoding/shiny/handler/GlobalExceptionHandler.java
@@ -5,9 +5,11 @@ import com.xkcoding.shiny.common.status.Status;
 import com.xkcoding.shiny.exception.ShinyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
@@ -33,6 +35,12 @@ public class GlobalExceptionHandler {
 		if (e instanceof NoHandlerFoundException) {
 			log.error("【全局异常拦截】NoHandlerFoundException: 请求方法 {}, 请求路径 {}", ((NoHandlerFoundException) e).getRequestURL(), ((NoHandlerFoundException) e).getHttpMethod());
 			return ApiResponse.ofStatus(Status.REQUEST_NOT_FOUND);
+		} else if (e instanceof MethodArgumentNotValidException) {
+			log.error("【全局异常拦截】MethodArgumentNotValidException", e);
+			return ApiResponse.ofStatus(Status.REQUEST_PARAMS_ERROR);
+		} else if (e instanceof MethodArgumentTypeMismatchException) {
+			log.error("【全局异常拦截】MethodArgumentTypeMismatchException: 参数名 {}, 异常信息 {}", ((MethodArgumentTypeMismatchException) e).getName(), ((MethodArgumentTypeMismatchException) e).getMessage());
+			return ApiResponse.ofStatus(Status.REQUEST_PARAMS_ERROR);
 		} else if (e instanceof HttpMessageNotReadableException) {
 			log.error("【全局异常拦截】HttpMessageNotReadableException: 错误信息 {}", ((HttpMessageNotReadableException) e).getMessage());
 			return ApiResponse.ofStatus(Status.REQUEST_PARAMS_ERROR);
