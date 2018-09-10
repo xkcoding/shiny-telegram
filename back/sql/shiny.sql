1022f677e9b3271081dd7b3ef3a3021ace5a53e0
Yangkai.Shen <237497819@qq.com>
2018-09-10 21:25:21 +0800

    :card_file_box: 执行数据库相关的变化

--- back/sql/shiny.sql
+++ back/sql/shiny.sql
@@ -1,3 +1,8 @@
+-- 创建数据库
+create database `shiny` default character set utf8 collate utf8_general_ci;
+
+use `shiny`;
+
 -- ----------------------------
 -- 1、参数配置表
 -- ----------------------------
@@ -7,7 +12,7 @@ CREATE TABLE `sys_config` (
   `config_name` varchar(100) DEFAULT '' COMMENT '参数名称',
   `config_key` varchar(100) DEFAULT '' COMMENT '参数键名',
   `config_value` varchar(100) DEFAULT '' COMMENT '参数键值',
-  `config_type` int(2) DEFAULT '0' COMMENT '系统内置（0否 1是）',
+  `config_type` int(2) DEFAULT 0 COMMENT '系统内置（0否 1是）',
   `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
@@ -28,11 +33,11 @@ CREATE TABLE `sys_user` (
   `user_type` varchar(2) DEFAULT '00' COMMENT '用户类型（00系统用户）',
   `email` varchar(50) DEFAULT '' COMMENT '用户邮箱',
   `phonenumber` varchar(11) DEFAULT '' COMMENT '手机号码',
-  `sex` int(2) DEFAULT '0' COMMENT '用户性别（0男 1女 2未知）',
+  `sex` int(2) DEFAULT 0 COMMENT '用户性别（0男 1女 2未知）',
   `avatar` varchar(100) DEFAULT '' COMMENT '头像路径',
   `password` varchar(100) DEFAULT '' COMMENT '密码',
-  `status` int(2) DEFAULT '1' COMMENT '帐号状态（0停用 1正常）',
-  `del_flag` int(2) DEFAULT '0' COMMENT '删除标志（0代表存在 1代表删除）',
+  `status` int(2) DEFAULT 1 COMMENT '帐号状态（0停用 1正常）',
+  `del_flag` int(2) DEFAULT 0 COMMENT '删除标志（0代表存在 1代表删除）',
   `login_ip` varchar(20) DEFAULT '' COMMENT '最后登陆IP',
   `login_date` datetime DEFAULT NULL COMMENT '最后登陆时间',
   `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
@@ -52,7 +57,7 @@ CREATE TABLE `sys_role` (
   `role_name` varchar(30) NOT NULL COMMENT '角色名称',
   `role_key` varchar(100) NOT NULL COMMENT '角色权限字符串',
   `role_sort` int(4) NOT NULL COMMENT '显示顺序',
-  `status` int(2) DEFAULT '1' COMMENT '角色状态（0停用 1正常）',
+  `status` int(2) DEFAULT 1 COMMENT '角色状态（0停用 1正常）',
   `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
@@ -93,29 +98,20 @@ CREATE TABLE `spider_config` (
 -- ----------------------------
 drop table if exists `spider_content`;
 CREATE TABLE `spider_content` (
-  `id`          int(11)      NOT NULL AUTO_INCREMENT
-  COMMENT '内容主键',
-  `config_id`   int(11)      NOT NULL
-  COMMENT '采集配置id',
-  `config_name` varchar(100) NOT NULL
-  COMMENT '采集配置名称',
-  `title`       varchar(100)          DEFAULT ''
-  COMMENT '软件名称',
-  `content`     text COMMENT '软件详细信息',
-  `version`     varchar(100)          DEFAULT ''
-  COMMENT '软件版本',
-  `language`    varchar(100)          DEFAULT ''
-  COMMENT '软件语言',
-  `update_time` date                  DEFAULT NULL COMMENT '软件更新时间',
-  `size`        varchar(100)          DEFAULT ''
-  COMMENT '软件大小',
-  `ct_pan_url`  varchar(500)          DEFAULT ''
-  COMMENT '城通网盘链接',
-  `ct_pan_code` varchar(500)          DEFAULT '' COMMENT '城通网盘提取码',
-  `bd_pan_url`  varchar(500)          DEFAULT ''
-  COMMENT '百度网盘链接',
-  `bd_pan_code` varchar(500)          DEFAULT '' COMMENT '百度网盘提取码',
-  `spider_time` datetime              DEFAULT NULL COMMENT '采集时间',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '内容主键',
+  `config_id` int(11) NOT NULL COMMENT '采集配置id',
+  `config_name` varchar(100) NOT NULL COMMENT '采集配置名称',
+  `title` varchar(100) DEFAULT '' COMMENT '软件名称',
+  `content` text COMMENT '软件详细信息',
+  `version` varchar(100) DEFAULT '' COMMENT '软件版本',
+  `language` varchar(100) DEFAULT '' COMMENT '软件语言',
+  `update_time` date DEFAULT NULL COMMENT '软件更新时间',
+  `size` varchar(100) DEFAULT '' COMMENT '软件大小',
+  `ct_pan_url` varchar(500) DEFAULT '' COMMENT '城通网盘链接',
+  `ct_pan_code` varchar(500) DEFAULT '' COMMENT '城通网盘提取码',
+  `bd_pan_url` varchar(500) DEFAULT '' COMMENT '百度网盘链接',
+  `bd_pan_code` varchar(500) DEFAULT '' COMMENT '百度网盘提取码',
+  `spider_time` datetime DEFAULT NULL COMMENT '采集时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='采集内容';
 
@@ -124,22 +120,14 @@ CREATE TABLE `spider_content` (
 -- ----------------------------
 drop table if exists `spider_log`;
 CREATE TABLE `spider_log` (
-  `id`          int(11) NOT NULL AUTO_INCREMENT
-  COMMENT '日志主键',
-  `config_id`   int(11) NOT NULL
-  COMMENT '采集配置 id',
-  `spider_name` varchar(100)     DEFAULT ''
-  COMMENT '采集名称',
-  `version`     varchar(100)     DEFAULT ''
-  COMMENT '采集版本',
-  `spider_url`  varchar(500)     DEFAULT ''
-  COMMENT '采集URL',
-  `status`      int(2)           DEFAULT '1'
-  COMMENT '采集状态（0异常 1正常）',
-  `error_msg`   varchar(2000)    DEFAULT ''
-  COMMENT '错误消息',
-  `spider_time` datetime         DEFAULT NULL
-  COMMENT '采集时间',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '日志主键',
+  `config_id` int(11) NOT NULL COMMENT '采集配置 id',
+  `spider_name` varchar(100) DEFAULT '' COMMENT '采集名称',
+  `version` varchar(100) DEFAULT '' COMMENT '采集版本',
+  `spider_url` varchar(500) DEFAULT '' COMMENT '采集URL',
+  `status` int(2) DEFAULT 1 COMMENT '采集状态（0异常 1正常）',
+  `error_msg` varchar(2000) DEFAULT '' COMMENT '错误消息',
+  `spider_time` datetime DEFAULT NULL COMMENT '采集时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='采集日志记录';
 
@@ -148,24 +136,13 @@ CREATE TABLE `spider_log` (
 -- ----------------------------
 drop table if exists `email_log`;
 CREATE TABLE `email_log` (
-  `id`            int(11) NOT NULL AUTO_INCREMENT
-  COMMENT '邮件主键',
-  `to`            text    NOT NULL
-  COMMENT '收件人邮箱地址（多个逗号分隔）',
-  `subject`       varchar(100)     DEFAULT ''
-  COMMENT '邮件主题',
-  `content`       text    NOT NULL
-  COMMENT '邮件内容',
-  `is_template`   int(2)           DEFAULT '1'
-  COMMENT '是否是模板邮件（0否 1是）',
-  `template_path` varchar(100)     DEFAULT NULL
-  COMMENT '模板路径',
-  `template_name` varchar(100)     DEFAULT NULL
-  COMMENT '模板名称',
-  `send_time`     datetime         DEFAULT NULL
-  COMMENT '邮件发送时间',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '邮件主键',
+  `to` text NOT NULL COMMENT '收件人邮箱地址（多个逗号分隔）',
+  `subject` varchar(100) DEFAULT '' COMMENT '邮件主题',
+  `content` text NOT NULL COMMENT '邮件内容',
+  `is_template` int(2) DEFAULT 1 COMMENT '是否是模板邮件（0否 1是）',
+  `template_path` varchar(100) DEFAULT NULL COMMENT '模板路径',
+  `template_name` varchar(100) DEFAULT NULL COMMENT '模板名称',
+  `send_time` datetime DEFAULT NULL COMMENT '邮件发送时间',
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8
-  COMMENT = '邮件记录';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='邮件记录';
