90f4749d7f1fabb7a65bfa6d1c356a7631bd79ad
Yangkai.Shen <237497819@qq.com>
2018-09-12 23:16:51 +0800

    :art: 前端项目结构调整

--- front/src/router/router.js
+++ front/src/router/router.js
@@ -1,4 +1,4 @@
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 export default [
   {
@@ -12,7 +12,6 @@ export default [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "@/views/About.vue")
   }
 ];
